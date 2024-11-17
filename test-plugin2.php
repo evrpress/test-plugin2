deb14ccd473ca4fa2288887347d1f70fb76dabf5
Xaver <xaver@everpress.co>
2024-11-17 14:39:34 +0100

    update

--- test-plugin2.php
+++ test-plugin2.php
@@ -9,7 +9,7 @@ namespace EverPress;
  * Description:       This is a simple Test plugin for...testing
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Version:           0.1.0
+ * Version:           0.1.3
  * Author:            Xaver
  * License:           GPL-2.0-or-later
  */
@@ -23,10 +23,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 // Initialize the updater
-\EverPress\WPUpdater::add(
+\EverPress\WPUpdater::add('test-plugin2/test-plugin2.php',
 	array(
 		'username'        => 'evrpress',
-		'repository'      => 'test-plugin',
+		'repository'      => 'test-plugin2',
 		'current_version' => '0.1.0',
 		'plugin_slug'     => 'test-plugin2/test-plugin2.php',
 	)

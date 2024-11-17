804d6b7a33f693bd9340be521e5e32f55eb6f150
Xaver <xaver@everpress.co>
2024-11-17 11:00:06 +0100

    first commit

---
+++ test-plugin2.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EverPress;
+
+
+
+/**
+ * Plugin Name:       Test Plugin
+ * Description:       This is a simple Test plugin for...testing
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Version:           0.1.0
+ * Author:            Xaver
+ * License:           GPL-2.0-or-later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+// Initialize the updater
+\EverPress\WPUpdater::add(
+	array(
+		'username'        => 'evrpress',
+		'repository'      => 'test-plugin',
+		'current_version' => '0.1.0',
+		'plugin_slug'     => 'test-plugin/test-plugin.php',
+	)
+);
+
+// Initialize the updater
+\EverPress\WPUpdater::add(
+	array(
+		'username'        => 'evrpress',
+		'repository'      => 'test-plugin',
+		'current_version' => '0.1.0',
+		'plugin_slug'     => 'test-plugin2/test-plugin2.php',
+	)
+);
